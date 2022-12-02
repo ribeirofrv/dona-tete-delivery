fdd83ea2b44ceba37075dc938c8bf2c37ee89f55
Fran Ribeiro <ribeirofrv@gmail.com>
2022-12-02 00:37:50 -0300

    fix(Back/api): renomeia arquivos

--- back-end/src/api/controllers/costumers.controller.js
+++
@@ -1,8 +0,0 @@
-const service = require('../services/costumer.service');
-
-const getAllProducts = async (_request, response, _next) => {
-  const allProducts = await service.getAllProducts();
-  return response.status(200).json(allProducts);
-};
-
-module.exports = { getAllProducts };
--- back-end/src/api/services/costumer.service.js
+++
@@ -1,8 +0,0 @@
-const { Product } = require('../../database/models');
-
-const getAllProducts = async () => {
-  const allProducts = await Product.findAll();
-  return allProducts;
-};
-
-module.exports = { getAllProducts };
