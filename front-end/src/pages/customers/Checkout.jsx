68f975990419b9799b45a9b955be9137edbbd9cf
Fran Ribeiro <ribeirofrv@gmail.com>
2022-12-02 00:51:05 -0300

    feat(Front/Pages): adiciona página base do checkout

---
+++ front-end/src/pages/customers/Checkout.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import Header from '../../components/Header';
+import OrdersBtn from '../../components/OrdersBtn';
+import ProductBtn from '../../components/ProductBtn';
+import Table from '../../components/Table';
+import CarTotalPrice from '../../components/carTotalPrice';
+
+export default function Checkout() {
+  const [cart, setCart] = useState([]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const { name } = localStorage.getItem('user');
+    const cartStorage = localStorage.getItem('carrinho');
+    setUsername(name);
+    setCart(cartStorage);
+  }, []);
+
+  return (
+    <>
+      <Header
+        FirstNavigationLink={ ProductBtn }
+        SecondNavegationLink={ OrdersBtn }
+        username={ username }
+      />
+      <h1>Finalizar Pedido</h1>
+      <Table data={ cart } />
+      <CarTotalPrice />
+    </>
+  );
+}
