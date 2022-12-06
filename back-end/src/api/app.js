6afa801b50331a852f71afe1256fec11eeb8a21e
Ckauark <ckauark@gmail.com>
2022-12-06 17:17:32 -0300

    fix: routes

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const errorMiddleware = require('./middlewares/errorMiddleware');
-const loginRouter = require('./routes/login.routes');
-const registerRouter = require('./routes/register.routes');
+const routes = require('./routes/index');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/login', loginRouter);
-app.use('/register', registerRouter);
+app.use('/login', routes.login);
+app.use('/register', routes.register);
 app.use(errorMiddleware);
 
 app.get('/tea', (_req, res) => res.status(418).end());
---
+++ back-end/src/api/routes/index.js
@@ -0,0 +1,4 @@
+const login = require('./login.routes');
+const register = require('./register.routes');
+
+module.exports = { login, register };
