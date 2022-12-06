4a952661a89954753fc1913ac99b67be0de1c067
erikadeolima <erikadeo.lima@hotmail.com>
2022-12-06 15:02:54 -0300

    fix: back-end/api/app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,18 +2,9 @@ const cors = require('cors');
 require('express-async-errors');
 const express = require('express');
 const errorMiddleware = require('./middlewares/errorMiddleware');
-const loginRouter = require('./routes/login.routes');
-
 const routes = require('./routes/index');
-const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use('/login', loginRouter);
-app.use(errorMiddleware);
-
 app.use(express.json());
 app.use(cors());
 
