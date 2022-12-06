9bbc1ff8e0e1b8188ddbd0a077762da6b7e3b269
erikadeolima <erikadeo.lima@hotmail.com>
2022-12-06 16:00:36 -0300

    feat create add to cart functions

--- front-end/src/components/carTotalPrice.js
+++ front-end/src/components/carTotalPrice.js
@@ -1,23 +1,36 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 // import { PropTypes } from 'prop-types';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { useHistory } from 'react-router-dom';
 
 function CarTotalPrice() {
-  // const history = useHistory();
-  const totalPriceData = useSelector((state) => state.reducer.totalPrice);
+  const { cart } = useContext(storage);
+  const [total, setTotal] = useState('0,00');
+
+  const redirectToCheckout = () => {
+    history.push('/customer/checkout');
+  };
 
   useEffect(() => {
-    dispatch(getProductsToState(''));
-  }, []);
+    if (cart.length > 0) {
+      const totalValue = cart
+        .reduce((acc, crr) => acc + Number(crr.subTotal.replace(/,/, '.')), 0);
+      setTotal(totalValue.toFixed(2));
+    }
+  }, [cart]);
+
   return (
-    <Link
-      data-testid="customer_products__checkout-bottom-value"
-      to="customer/checkout"
+    <button
+      type="button"
+      data-testid="customer_products__button-cart"
+      className="cart-btn"
+      onClick={ () => redirectToCheckout() }
+      disabled={ total === '0,00' || total === '0.00' ? 1 : 0 }
     >
-      {totalPriceData}
-    </Link>
+      <span data-testid="customer_products__checkout-bottom-value">
+        { ` R$ ${cart ? total.replace(/\./, ',') : 0}` }
+      </span>
+    </button>
   );
 }
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
