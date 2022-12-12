6cde990b5e2a38189d72af83762729f315b53dad
Fran Ribeiro <ribeirofrv@gmail.com>
2022-12-12 15:54:36 -0300

    feat(Back): (tentando) adiciona imagens

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 require('express-async-errors');
 const express = require('express');
-const errorMiddleware = require('./middlewares/error.middleware');
+
 const routes = require('./routes/index');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
@@ -9,12 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static('public')); // deveria expor images
+
 app.use('/login', routes.login);
 app.use('/register', routes.register);
 app.use('/customer', routes.customer);
 app.use('/seller', routes.seller);
-
-app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(errorMiddleware);
 
