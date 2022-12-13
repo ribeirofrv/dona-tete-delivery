2774607f25c96f8ea52705582a54c3b1dd460ff5
erikadeolima <erikadeo.lima@hotmail.com>
2022-12-13 17:51:50 -0300

    feat: requisição de sellers no back

--- back-end/src/api/routes/customer.routes.js
+++ back-end/src/api/routes/customer.routes.js
@@ -5,5 +5,6 @@ const customerController = require('../controllers/customer.controller');
 const customerRoute = Router();
 
 customerRoute.get('/products', customerController.getAllProducts);
+customerRoute.get('/checkout', customerController.getAllSeller);
 
 module.exports = customerRoute;
