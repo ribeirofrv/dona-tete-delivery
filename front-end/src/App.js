cefe27b142f7c221a8b9640346fd4cb3fe5a566c
Fran Ribeiro <ribeirofrv@gmail.com>
2022-12-13 22:27:25 -0300

    feat(Front/app): altera Provider do ContextAPI para rota principal

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Provider from './context/provider';
+
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Seller from './pages/seller/Seller';
 import Products from './pages/customers/Products';
 import Checkout from './pages/customers/Checkout';
+import Seller from './pages/seller/Seller';
 import SellerDetails from './pages/seller/SellerDetails';
-import Home from './pages/Home';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route exact path="/login" component={ Login } />
-      <Route path="/register" component={ Register } />
-      <Route exact path="/customer/products" component={ Products } />
-      <Route exact path="/customer/checkout" component={ Checkout } />
-      <Route exact path="/seller/orders" component={ Seller } />
-      <Route exact path="/seller/orders/:id" component={ SellerDetails } />
-    </Switch>
+    <Provider>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/login" component={ Login } />
+        <Route path="/register" component={ Register } />
+        <Route exact path="/customer/products" component={ Products } />
+        <Route exact path="/customer/checkout" component={ Checkout } />
+        <Route exact path="/seller/orders" component={ Seller } />
+        <Route exact path="/seller/orders/:id" component={ SellerDetails } />
+      </Switch>
+    </Provider>
   );
 }
 
