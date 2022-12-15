4c897e33b4d4902fecd39eed24841bd14626b0cf
Loly Maluf <lolymaluf@gmail.com>
2022-12-15 18:14:27 -0300

    Comentarios  p/ refatorar caso precise nos testes

--- front-end/src/API/requests.js
+++ front-end/src/API/requests.js
@@ -34,5 +34,6 @@ export const requestLogin = async (endpoint, body) => {
   const { data } = await api.post(endpoint, body);
   return data;
 };
+// tratar erro na requestLogin
 
 export default api;
